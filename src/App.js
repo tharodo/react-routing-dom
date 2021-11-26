7af0b3a03bb6a0e1df5206bff996c301d641fb26
thao phuong <dophuongthao.459@gmail.com>
2021-11-27 00:45:52 +0700

    Add Switch exact

--- src/App.js
+++ src/App.js
@@ -12,18 +12,21 @@ function App() {
     <Router>
       <div className="App">
         <Nav/>
-        <Route
-          path="/"
-          component={Home}
-        />
-        <Route
-          path="/about" 
-          component={About}
-        />
-        <Route
-          path="/shop"
-          component={Shop}
-        />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={Home}
+          />
+          <Route
+            path="/about" 
+            component={About}
+          />
+          <Route
+            path="/shop"
+            component={Shop}
+          />
+        </Switch>
       </div>    
     </Router>
   );
