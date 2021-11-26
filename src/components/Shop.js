ce6f4690857bd6689fda3572e35f513bc51c3371
thao phuong <dophuongthao.459@gmail.com>
2021-11-27 01:24:48 +0700

    Fetch API

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
 
 function Shop() {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetchItems();
+    }, [])
+
+    const fetchItems = async () => {
+        const data = await fetch('https://fakestoreapi.com/products');
+        const items = await data.json();
+        console.log(items);
+        setItems(items);
+    }
+
     return (
         <div>
-            <h1>Shop</h1>
+            {items.map(item => (
+                <div key={item.id}>
+                    <h3>
+                        {item.title}
+                    </h3>                    
+                </div>
+                
+            ))}            
         </div>
     );
 }
