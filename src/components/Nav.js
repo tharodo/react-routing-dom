516efab726418a70dd1cf428255de7acc29cbdf5
thao phuong <dophuongthao.459@gmail.com>
2021-11-27 00:58:58 +0700

    Add Link to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import style from './nav.module.css'
+import { Link } from 'react-router-dom'
 
 function Nav() {
+
+    const styleLink = {
+        color: 'white',
+        textDecoration: 'none',
+        fontWeight: 'bold'
+    }
+
     return (
         <nav className={style.styleNav}>
             <h3>Logo</h3>
             <ul className={style.navLinks}>
-                <li>About</li>
-                <li>Shop</li>
+                <Link style={styleLink} to="/about" >
+                    <li>About</li>
+                </Link>
+                <Link style={styleLink} to="/shop">
+                    <li>Shop</li>
+                </Link>
             </ul>
         </nav>
     );
